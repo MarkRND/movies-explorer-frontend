359055394cb47389db92078aff4343d46af92b2b
Gorlov Mark <masmik@rambler.ru>
2023-08-05 14:14:07 +0300

    исправлен Link 404

--- src/components/ErrorContent/ErrorContent.jsx
+++ src/components/ErrorContent/ErrorContent.jsx
@@ -3,14 +3,12 @@ import { Link } from "react-router-dom";
 import "./ErrorContent.css";
 
 const ErrorContent = () => {
-  const handleGoBack = () => {
-    window.history.back();
-  };
+  
   return (
     <main className="error">
       <h1 className="error__title">404</h1>
       <p className="error__message">Страница не найдена</p>
-      <Link className="error__link" onClick={handleGoBack}>
+      <Link className="error__link" to="/">
         Назад
       </Link>
     </main>
